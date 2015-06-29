856f794282933a6aaa3c1fcc98c019bf47de4101
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-06-28 17:33:13 -0700

    pep8

--- garden.py
+++ garden.py
@@ -13,24 +13,34 @@ JABBER_ID = 'aleckkozak@gmail.com'
 JABBER_PASSWORD = 'sixhcuaajyjbykaw'
 SHORT_ID = 'oneled'
 FRIENDLY_NAME = 'Raised Bed 1'
+MIN_TEMP, MAX_TEMP, DEFAULT_TEMP = 20, 110, 70
 PANEL_FORM = [
-             [ ['O'] ],
-             [ ['S', 'weather', '-'], ['G','temp','temp',70,20,110] ],
-             [ ['C'] ],
-             [ ['O'] ],
-             [ ['L','Soaker'],['B','on_button','On'],['B','off_button','Off'],['S','state','-'] ],
-             [ ['C'] ],
-             ]
+    [['O']],
+    [
+        ['S', 'weather', '-'],
+        ['G', 'temp', 'temp', DEFAULT_TEMP, MIN_TEMP, MAX_TEMP]
+    ],
+    [['C']],
+    [['O']],
+    [
+        ['L', 'Soaker'],
+        ['B', 'on_button', 'On'],
+        ['B', 'off_button', 'Off'],
+        ['S', 'state', '-'],
+    ],
+    [['C']],
+]
 
 GPIO_NUM = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_NUM, GPIO.OUT)
 
-led_state = {GPIO_NUM:False}
+led_state = {GPIO_NUM: False}
+
 
 def switch_led(state):
     if led_state[GPIO_NUM] != state:
-        GPIO.output(GPIO_NUM, state) #High to glow!
+        GPIO.output(GPIO_NUM, state)  # High to glow!
         conn.update_status({'state': 'watering' if state else 'off'})
         led_state[GPIO_NUM] = state
     global start_time
@@ -38,17 +48,22 @@ def switch_led(state):
         start_time = time.time()
         update_weather()
 
+
 def on_control_message(conn, key, value):
     if key == 'on_button':
         switch_led(True)
     elif key == 'off_button':
         switch_led(False)
 
+
 def update_weather():
     resp = requests.get(URL)
     if not resp.ok:
         print "error getting weather data", resp.content
-        conn.update_status({'weather': 'error getting weather data', 'temp': 30})
+        conn.update_status({
+            'weather': 'error getting weather data',
+            'temp': 30
+        })
     else:
         data = resp.json()
         summary = data['currently']['summary']
@@ -60,10 +75,18 @@ def main_loop():
     switch_led(True)
     update_weather()
     while True:
-        time.sleep(3) #Yield for a while but keep main thread running
+        time.sleep(3)  # Yield for a while but keep main thread running
 
 
-conn = ControlMyPi(JABBER_ID, JABBER_PASSWORD, SHORT_ID, FRIENDLY_NAME, PANEL_FORM, on_control_message)
+conn = ControlMyPi(
+    JABBER_ID,
+    JABBER_PASSWORD,
+    SHORT_ID,
+    FRIENDLY_NAME,
+    PANEL_FORM,
+    on_control_message
+)
+
 if conn.start_control():
     try:
         main_loop()
