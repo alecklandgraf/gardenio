7b49fc21fbdebfd79ba34a04bdc68ca26232b33d
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-06-30 22:21:09 -0700

    slight refactor

--- garden.py
+++ garden.py
@@ -1,6 +1,7 @@
 from controlmypi import ControlMyPi
 import RPi.GPIO as GPIO
 import time
+import datetime
 import requests
 import os
 from twilio.rest import TwilioRestClient
@@ -10,7 +11,7 @@ URL = (
     'https://api.forecast.io/forecast/{}/45.5119,-122.5943'
 ).format(os.environ.get('FORCASTIO_KEY'))
 
-weather_start_time = time.time()
+
 JABBER_ID = os.environ.get('JABBER_ID')
 JABBER_PASSWORD = os.environ.get('JABBER_PASSWORD')
 SHORT_ID = 'oneled'
@@ -19,6 +20,7 @@ TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
 TO_NUMBER = os.environ.get('TO_NUMBER')
 FROM_NUMBER = os.environ.get('FROM_NUMBER')
 client = TwilioRestClient(TWILIO_SID, TWILIO_TOKEN)
+ONE_HOUR = 60 * 60
 
 FRIENDLY_NAME = 'Raised Bed 1'
 MIN_TEMP, MAX_TEMP, DEFAULT_TEMP = 20, 110, 70
@@ -61,10 +63,6 @@ def switch_led(state):
         GPIO.output(GPIO_VALVE_PIN, state)  # High to glow!
         conn.update_status({'state': 'watering' if state else 'off'})
         garden_state[GPIO_VALVE_PIN] = state
-    global weather_start_time
-    if (time.time() - weather_start_time) > 5 * 60:
-        weather_start_time = time.time()
-        update_weather()
 
 
 def on_control_message(conn, key, value):
@@ -74,53 +72,65 @@ def on_control_message(conn, key, value):
         switch_led(False)
 
 
-def update_moisture_reading(channel=None):
-    print "updating moisture reading"
-    GPIO.output(GPIO_MOISTURE_POWER_PIN, GPIO.HIGH)
-    time.sleep(.5)
-    reading = GPIO.input(GPIO_MOISTURE_INPUT_PIN)
-    # check initial None state
-    if reading == garden_state[GPIO_MOISTURE_INPUT_PIN]:
-        print "nothing changed"
-    elif reading:
-        conn.update_status({'moisture': 'Soil dry'})
-        garden_state[GPIO_MOISTURE_INPUT_PIN] = reading
-        print "Soil Dry"
-        # client.messages.create(
-        #     to="+{}".format(TO_NUMBER),
-        #     from_="+{}".format(FROM_NUMBER),
-        #     body="water me"
-        # )
+def update_moisture_reading(start_time=None, refresh_threshold_sec=ONE_HOUR):
+    if start_time and (time.time() - start_time) < refresh_threshold_sec:
+        return start_time
     else:
-        conn.update_status({'moisture': 'Soil OK!'})
-        print "Soil Ok"
-        garden_state[GPIO_MOISTURE_INPUT_PIN] = reading
-    GPIO.output(GPIO_MOISTURE_POWER_PIN, GPIO.LOW)
+        start_time = time.time()
+        now = datetime.datetime.now()
+        GPIO.output(GPIO_MOISTURE_POWER_PIN, GPIO.HIGH)
+        # tiny delay to let the sensor pull low
+        time.sleep(1)
+        reading = GPIO.input(GPIO_MOISTURE_INPUT_PIN)
+        # check initial None state
+        if reading == garden_state[GPIO_MOISTURE_INPUT_PIN]:
+            print "{} nothing changed".format(now)
+        elif reading:
+            conn.update_status({'moisture': '{} Soil dry'.format(now)})
+            garden_state[GPIO_MOISTURE_INPUT_PIN] = reading
+            print "{} Soil Dry".format(now)
+            client.messages.create(
+                to="+{}".format(TO_NUMBER),
+                from_="+{}".format(FROM_NUMBER),
+                body="water me"
+            )
+        else:
+            conn.update_status({'moisture': '{} Soil OK!'.format(now)})
+            print "{} Soil Ok".format(now)
+            garden_state[GPIO_MOISTURE_INPUT_PIN] = reading
+        GPIO.output(GPIO_MOISTURE_POWER_PIN, GPIO.LOW)
+        return start_time
 
 
-def update_weather():
-    resp = requests.get(URL)
-    if not resp.ok:
-        print "error getting weather data", resp.content
-        conn.update_status({
-            'weather': 'error getting weather data',
-            'temp': 30
-        })
+def update_weather(start_time=None, refresh_threshold_sec=ONE_HOUR):
+    if start_time and (time.time() - start_time) < refresh_threshold_sec:
+        return start_time
     else:
-        data = resp.json()
-        summary = data['currently']['summary']
-        temp = data['currently']['temperature']
-        conn.update_status({'weather': '{}'.format(summary), 'temp': temp})
+        start_time = time.time()
+        resp = requests.get(URL)
+        if not resp.ok:
+            print "error getting weather data", resp.content
+            conn.update_status({
+                'weather': 'error getting weather data',
+                'temp': 30
+            })
+        else:
+            data = resp.json()
+            summary = data['currently']['summary']
+            temp = data['currently']['temperature']
+            conn.update_status({'weather': '{}'.format(summary), 'temp': temp})
+        return start_time
 
 
 def main_loop():
     switch_led(False)
     # run once
-    update_moisture_reading()
-    update_weather()
+    moisture_start = update_moisture_reading()
+    weather_start = update_weather()
     while True:
         time.sleep(3)  # Yield for a while but keep main thread running
-        update_moisture_reading()
+        moisture_start = update_moisture_reading(start_time=moisture_start)
+        weather_start = update_weather(start_time=weather_start)
 
 
 conn = ControlMyPi(
@@ -132,12 +142,6 @@ conn = ControlMyPi(
     on_control_message
 )
 
-# this has to be declared below the callback
-# GPIO.add_event_detect(
-#     GPIO_MOISTURE_INPUT_PIN,
-#     GPIO.BOTH,
-#     callback=update_moisture_reading
-# )
 if conn.start_control():
     try:
         main_loop()
